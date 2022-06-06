c7e6709ac3037099a08afe496f0e746fb602552e
Brian Wangila <bwanjalah@gmail.com>
2022-06-06 22:19:41 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-function scuberGreetingForFeet(){
+let feet;
+function scuberGreetingForFeet(feet){
   // Write your code here!
-}
+  if (feet <= 400){
+    return "This one is on me!"
+  }
+  else if (feet > 2000 && feet <= 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return "No can do."
+  }
 
-function ternaryCheckCity(){
-  // Write your code here!
 }
+scuberGreetingForFeet(200)
 
-function switchOnCharmFromTip(){
+
+let city;
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else if (city !== "NYC") {
+    return "No go."
+  }
 }
+ternaryCheckCity("NYC")
+
+
+let tip;
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch (tip){
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+      break;
+    case "thanks for everything":
+      return "Bye."
+    
+  }
+
+}
+console.log(switchOnCharmFromTip("Thank you."))
